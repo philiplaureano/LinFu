e20c2b53c219cf147ca59cf73d520aee3a208368
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2007-12-05 10:22:24 +0000

    Added locking semantics to DefaultContractStorage.cs.
    git-svn-id: http://linfu.googlecode.com/svn/trunk@19 228fa388-fe3e-0410-9462-c1833096a2b3

--- LinFu.DesignByContract2/LinFu.DesignByContract2.Injectors/DefaultContractStorage.cs
+++ LinFu.DesignByContract2/LinFu.DesignByContract2.Injectors/DefaultContractStorage.cs
@@ -9,22 +9,37 @@ namespace LinFu.DesignByContract2.Injectors
     [Implements(typeof(IContractStorage), LifecycleType.Singleton)]
     public class DefaultContractStorage : IContractStorage 
     {
-        private Dictionary<Type, IContractSource> _contractTypes = new Dictionary<Type, IContractSource>();
+        private readonly Dictionary<Type, IContractSource> _contractTypes = new Dictionary<Type, IContractSource>();
+        private readonly object lockObject = new object();
         #region IContractStorage Members
 
         public bool HasContractFor(Type targetType)
         {
-            return _contractTypes.ContainsKey(targetType);
+            bool result;
+
+            lock (lockObject)
+            {
+                result = _contractTypes.ContainsKey(targetType);
+            }
+            return result;
         }
 
         public IContractSource GetContractTypeFor(Type targetType)
         {
-            return _contractTypes[targetType];
+            IContractSource result = null;
+            lock (lockObject)
+            {
+                result = _contractTypes[targetType];
+            }
+            return result;
         }
 
         public void AddContractType(Type targetType, IContractSource contractSourceType)
         {
-            _contractTypes[targetType] = contractSourceType;
+            lock (lockObject)
+            {
+                _contractTypes[targetType] = contractSourceType;
+            }
         }
 
         #endregion
