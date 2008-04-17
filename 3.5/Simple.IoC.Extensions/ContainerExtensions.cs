70f160824226c95f6ff61bfe4e37d37340404809
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-04-17 04:33:27 +0000

    Added a few extension methods to Simple.IOC
    git-svn-id: http://linfu.googlecode.com/svn/trunk@92 228fa388-fe3e-0410-9462-c1833096a2b3

---
+++ 3.5/Simple.IoC.Extensions/ContainerExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.IoC.Extensions
+{
+    public static class ContainerExtensions
+    {
+        public static void AddNamedService<T>(this IContainer container, string serviceName,
+            Func<T> factoryMethod, params Action<T>[] initializeActions)
+            where T : class
+        {
+            var initialize = CombineActions<T>(initializeActions);
+
+            var surrogate = new FactorySurrogate<T>(serviceName, factoryMethod, initialize);
+            container.TypeSurrogates.Add(surrogate);
+        }
+
+        public static void AddCustomizationStep<T>(this IContainer container, string serviceName,
+            params Action<T>[] initializeActions)
+            where T : class
+        {
+            var initialize = CombineActions<T>(initializeActions);
+            NamedCustomizer<T> customizer = new NamedCustomizer<T>(serviceName, initialize);
+            container.Customizers.Add(customizer);
+        }
+        private static Action<T> CombineActions<T>(Action<T>[] initializeActions) where T : class
+        {
+            // Combine all the initialization steps
+            // into a single action
+            Action<T> initialize = delegate { };
+            foreach (var action in initializeActions)
+            {
+                initialize += action;
+            }
+            return initialize;
+        }
+    }
+}
