7fb42a5727d3f32041a07b1183d7af6c2297c6eb
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-02-27 04:28:37 +0000

    Modified the PEVerifier to skip the test if PEVerify cannot be found.
    git-svn-id: http://linfu.googlecode.com/svn/trunk@375 228fa388-fe3e-0410-9462-c1833096a2b3

--- src/UnitTests/Tools/PEVerifier.cs
+++ src/UnitTests/Tools/PEVerifier.cs
@@ -54,9 +54,12 @@ namespace LinFu.UnitTests.Tools
 
             peVerifyLocation = GetPEVerifyLocation(pathKeys, peVerifyLocation);
 
+            // Use PEVerify.exe if it exists
             if (!File.Exists(peVerifyLocation))
-                throw new FileNotFoundException(
-                    "Please check the sdkDir configuration setting and set it to the location of peverify.exe");
+            {
+                Console.WriteLine("Warning: PEVerify.exe could not be found. Skipping test.");
+                return;
+            }
 
             process.StartInfo.FileName = peVerifyLocation;
             process.StartInfo.RedirectStandardOutput = true;
