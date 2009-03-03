73c46e0b86d26d87a3a4c450d774fc039cb08a25
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-03-03 04:16:17 +0000

    git-svn-id: http://linfu.googlecode.com/svn/trunk@377 228fa388-fe3e-0410-9462-c1833096a2b3

---
+++ src/LinFu.IoC/Configuration/Interfaces/IContainerActivationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinFu.AOP.Interfaces;
+using LinFu.IoC.Interfaces;
+
+namespace LinFu.IoC.Configuration.Interfaces
+{
+    /// <summary>
+    /// Represents a class that describes a request to instantiate a particular object type using a given
+    /// <see cref="IServiceContainer"/> instance.
+    /// </summary>
+    public interface IContainerActivationContext : IActivationContext 
+    {
+        /// <summary>
+        /// Gets the value indicating the <see cref="IServiceContainer"/> instance
+        /// that will instantiate the <see cref="IActivationContext.TargetType"/>.
+        /// </summary>
+        IServiceContainer Container { get; }
+    }
+}
