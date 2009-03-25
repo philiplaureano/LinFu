01b31c1e6560f1d6e9adbb123a009a9ec014815b
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-03-25 00:11:53 +0000

    Changes (trunk): Updated the BaseReflectionVisitor class to traverse an entire AssemblyDefinition by default.
    git-svn-id: http://linfu.googlecode.com/svn/trunk@383 228fa388-fe3e-0410-9462-c1833096a2b3

--- src/LinFu.AOP/BaseReflectionVisitor.cs
+++ src/LinFu.AOP/BaseReflectionVisitor.cs
@@ -17,15 +17,7 @@ namespace LinFu.AOP.Cecil
         {
         }
 
-        public virtual void VisitConstructorCollection(ConstructorCollection ctors)
-        {
-        }
-
         public virtual void VisitCustomAttribute(CustomAttribute customAttr)
-        {
-        }
-
-        public virtual void VisitCustomAttributeCollection(CustomAttributeCollection customAttrs)
         {
         }
 
@@ -33,15 +25,7 @@ namespace LinFu.AOP.Cecil
         {
         }
 
-        public virtual void VisitEventDefinitionCollection(EventDefinitionCollection events)
-        {
-        }
-
         public virtual void VisitExternType(TypeReference externType)
-        {
-        }
-
-        public virtual void VisitExternTypeCollection(ExternTypeCollection externs)
         {
         }
 
@@ -49,23 +33,11 @@ namespace LinFu.AOP.Cecil
         {
         }
 
-        public virtual void VisitFieldDefinitionCollection(FieldDefinitionCollection fields)
-        {
-        }
-
         public virtual void VisitGenericParameter(GenericParameter genparam)
         {
         }
 
-        public virtual void VisitGenericParameterCollection(GenericParameterCollection genparams)
-        {
-        }
-
         public virtual void VisitInterface(TypeReference interf)
-        {
-        }
-
-        public virtual void VisitInterfaceCollection(InterfaceCollection interfaces)
         {
         }
 
@@ -77,35 +49,32 @@ namespace LinFu.AOP.Cecil
         {
         }
 
-        public virtual void VisitMemberReferenceCollection(MemberReferenceCollection members)
-        {
-        }
-
-        public virtual void VisitMethodDefinition(MethodDefinition method)
-        {
-        }
-
-        public virtual void VisitMethodDefinitionCollection(MethodDefinitionCollection methods)
-        {
-        }
-
         public virtual void VisitModuleDefinition(ModuleDefinition module)
         {
-        }
-
-        public virtual void VisitNestedType(TypeDefinition nestedType)
-        {
+            VisitCustomAttributeCollection(module.CustomAttributes);
+            VisitResourceCollection(module.Resources);
+            VisitAssemblyNameReferenceCollection(module.AssemblyReferences);
+            VisitModuleReferenceCollection(module.ModuleReferences);
+            VisitMemberReferenceCollection(module.MemberReferences);
+            VisitTypeReferenceCollection(module.TypeReferences);
+            VisitTypeDefinitionCollection(module.Types);
+            VisitExternTypeCollection(module.ExternTypes);
+            TerminateModuleDefinition(module);
         }
 
         public virtual void VisitNestedTypeCollection(NestedTypeCollection nestedTypes)
         {
+            foreach (TypeDefinition nestedType in nestedTypes)
+            {
+                VisitNestedType(nestedType);
+            }
         }
 
         public virtual void VisitOverride(MethodReference ov)
         {
         }
 
-        public virtual void VisitOverrideCollection(OverrideCollection meth)
+        public virtual void VisitSecurityDeclaration(SecurityDeclaration secDecl)
         {
         }
 
@@ -117,39 +86,15 @@ namespace LinFu.AOP.Cecil
         {
         }
 
-        public virtual void VisitParameterDefinitionCollection(ParameterDefinitionCollection parameters)
-        {
-        }
-
         public virtual void VisitPropertyDefinition(PropertyDefinition property)
         {
         }
 
-        public virtual void VisitPropertyDefinitionCollection(PropertyDefinitionCollection properties)
-        {
-        }
-
-        public virtual void VisitSecurityDeclaration(SecurityDeclaration secDecl)
-        {
-        }
-
-        public virtual void VisitSecurityDeclarationCollection(SecurityDeclarationCollection secDecls)
-        {
-        }
-
-        public virtual void VisitTypeDefinition(TypeDefinition type)
-        {
-        }
-
-        public virtual void VisitTypeDefinitionCollection(TypeDefinitionCollection types)
+        public virtual void VisitNestedType(TypeDefinition nestedType)
         {
         }
 
         public virtual void VisitTypeReference(TypeReference type)
-        {
-        }
-
-        public virtual void VisitTypeReferenceCollection(TypeReferenceCollection refs)
         {
         }
 
@@ -159,6 +104,11 @@ namespace LinFu.AOP.Cecil
 
         public virtual void VisitAssemblyDefinition(AssemblyDefinition asm)
         {
+            VisitCustomAttributeCollection(asm.CustomAttributes);
+            VisitModuleDefinitionCollection(asm.Modules);
+            VisitSecurityDeclarationCollection(asm.SecurityDeclarations);
+
+            TerminateAssemblyDefinition(asm);
         }
 
         public virtual void VisitAssemblyLinkedResource(AssemblyLinkedResource res)
@@ -173,10 +123,6 @@ namespace LinFu.AOP.Cecil
         {
         }
 
-        public virtual void VisitAssemblyNameReferenceCollection(AssemblyNameReferenceCollection names)
-        {
-        }
-
         public virtual void VisitEmbeddedResource(EmbeddedResource res)
         {
         }
@@ -184,22 +130,186 @@ namespace LinFu.AOP.Cecil
         public virtual void VisitLinkedResource(LinkedResource res)
         {
         }
-
-        public virtual void VisitModuleDefinitionCollection(ModuleDefinitionCollection modules)
-        {
-        }
-
         public virtual void VisitModuleReference(ModuleReference module)
         {
         }
 
+        #region Collection Visitors
+        public virtual void VisitTypeDefinition(TypeDefinition type)
+        {
+            // Visit the interfaces
+            VisitInterfaceCollection(type.Interfaces);
+
+            // Visit the constructors
+            VisitConstructorCollection(type.Constructors);
+
+            // Visit the fields
+            VisitFieldDefinitionCollection(type.Fields);
+
+            // Visit the methods
+            VisitMethodDefinitionCollection(type.Methods);
+
+            // Visit the properties
+            VisitPropertyDefinitionCollection(type.Properties);
+
+            // Visit the events
+            VisitEventDefinitionCollection(type.Events);
+
+            // Visit the nested types
+            VisitNestedTypeCollection(type.NestedTypes);
+
+            // Visit the security declaration
+            VisitSecurityDeclarationCollection(type.SecurityDeclarations);
+        }
+        public virtual void VisitConstructorCollection(ConstructorCollection ctors)
+        {
+            foreach (MethodDefinition ctor in ctors)
+            {
+                VisitConstructor(ctor);
+            }
+        }        
+        public virtual void VisitCustomAttributeCollection(CustomAttributeCollection customAttrs)
+        {
+            foreach (CustomAttribute ca in customAttrs)
+            {
+                VisitCustomAttribute(ca);
+            }
+        }
+        public virtual void VisitEventDefinitionCollection(EventDefinitionCollection events)
+        {
+            foreach (EventDefinition eventDef in events)
+            {
+                VisitEventDefinition(eventDef);
+            }
+        }
+        public virtual void VisitExternTypeCollection(ExternTypeCollection externs)
+        {
+            foreach (TypeReference type in externs)
+            {
+                VisitExternType(type);
+            }
+        }
+        public virtual void VisitFieldDefinitionCollection(FieldDefinitionCollection fields)
+        {
+            foreach (FieldDefinition field in fields)
+            {
+                VisitFieldDefinition(field);
+            }
+        }
+        public virtual void VisitGenericParameterCollection(GenericParameterCollection genparams)
+        {
+            foreach (GenericParameter param in genparams)
+            {
+                VisitGenericParameter(param);
+            }
+        }
+        public virtual void VisitInterfaceCollection(InterfaceCollection interfaces)
+        {
+            foreach (TypeReference interfaceType in interfaces)
+            {
+                VisitInterface(interfaceType);
+            }
+        }        
+        public virtual void VisitMemberReferenceCollection(MemberReferenceCollection members)
+        {
+            foreach (MemberReference memberRef in members)
+            {
+                VisitMemberReference(memberRef);
+            }
+        }
+        public virtual void VisitMethodDefinition(MethodDefinition method)
+        {
+            // Visit the parameters
+            VisitParameterDefinitionCollection(method.Parameters);
+            
+            // Visit the generic parameters
+            VisitGenericParameterCollection(method.GenericParameters);
+
+            // Visit the method overrides
+            VisitOverrideCollection(method.Overrides);
+            
+            // Visit the security declarations
+            VisitSecurityDeclarationCollection(method.SecurityDeclarations);
+        }
+        public virtual void VisitMethodDefinitionCollection(MethodDefinitionCollection methods)
+        {
+            foreach (MethodDefinition method in methods)
+            {
+                VisitMethodDefinition(method);
+            }
+        } 
+        public virtual void VisitOverrideCollection(OverrideCollection overrides)
+        {
+            foreach (MethodReference method in overrides)
+            {
+                VisitOverride(method);
+            }
+        }
+        public virtual void VisitParameterDefinitionCollection(ParameterDefinitionCollection parameters)
+        {
+            foreach (ParameterDefinition param in parameters)
+            {
+                VisitParameterDefinition(param);
+            }
+        }
+        public virtual void VisitPropertyDefinitionCollection(PropertyDefinitionCollection properties)
+        {
+            foreach (PropertyDefinition property in properties)
+            {
+                VisitPropertyDefinition(property);
+            }
+        }
+        public virtual void VisitSecurityDeclarationCollection(SecurityDeclarationCollection secDecls)
+        {
+            foreach (SecurityDeclaration securityDeclaration in secDecls)
+            {
+                VisitSecurityDeclaration(securityDeclaration);
+            }
+        }
+
+        public virtual void VisitTypeDefinitionCollection(TypeDefinitionCollection types)
+        {
+            foreach (TypeDefinition typeDef in types)
+            {
+                VisitTypeDefinition(typeDef);
+            }
+        }
+       
+        public virtual void VisitTypeReferenceCollection(TypeReferenceCollection refs)
+        {
+            foreach (TypeReference type in refs)
+            {
+                VisitTypeReference(type);
+            }
+        }
+       
+        public virtual void VisitAssemblyNameReferenceCollection(AssemblyNameReferenceCollection names)
+        {
+            foreach (AssemblyNameReference nameRef in names)
+            {
+                VisitAssemblyNameReference(nameRef);
+            }
+        }        
+        public virtual void VisitModuleDefinitionCollection(ModuleDefinitionCollection modules)
+        {
+            foreach (ModuleDefinition module in modules)
+            {
+                VisitModuleDefinition(module);
+            }
+        }
+
         public virtual void VisitModuleReferenceCollection(ModuleReferenceCollection modules)
         {
+            foreach (ModuleReference module in modules)
+            {
+                VisitModuleReference(module);
+            }
         }
 
         public virtual void VisitResourceCollection(ResourceCollection resources)
         {
         }
+        #endregion
     }
 }
 #pragma warning restore 1591
