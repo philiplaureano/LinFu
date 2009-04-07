fb553b31e8448e0afb975ab4c72d5926c27f7857
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-04-07 10:33:54 +0000

    Modified the trunk revision to 2.0.4 to reflect the changes in LinFu
    git-svn-id: http://linfu.googlecode.com/svn/trunk@391 228fa388-fe3e-0410-9462-c1833096a2b3

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.3.*")]
-[assembly: AssemblyFileVersion("2.0.3.2")]
+[assembly: AssemblyVersion("2.0.4.*")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
