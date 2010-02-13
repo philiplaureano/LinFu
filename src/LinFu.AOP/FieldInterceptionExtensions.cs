059027193a03bb1493caac34f18180db4b2dba89
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2010-02-13 02:15:29 +0000

    Added missing files
    git-svn-id: http://linfu.googlecode.com/svn/trunk@482 228fa388-fe3e-0410-9462-c1833096a2b3

---
+++ src/LinFu.AOP/FieldInterceptionExtensions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+namespace LinFu.AOP.Cecil
+{
+    /// <summary>
+    /// Represents an extension class that adds field interception support to a given type.
+    /// </summary>
+    public static class FieldInterceptionExtensions
+    {
+        /// <summary>
+        /// Adds field interception support to the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>        
+        public static void InterceptAllFields(this IReflectionStructureVisitable targetType)
+        {
+            var methodFilter = GetMethodFilter();
+            targetType.InterceptFields(methodFilter, f => true);
+        }
+
+        /// <summary>
+        /// Adds field interception support intercepting all instance fields on the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>        
+        public static void InterceptAllInstanceFields(this IReflectionStructureVisitable targetType)
+        {
+            var methodFilter = GetMethodFilter();
+            Func<FieldReference, bool> fieldFilter = field =>
+            {
+                var actualField = field.Resolve();
+                return !actualField.IsStatic;
+            };
+
+            targetType.InterceptFields(methodFilter, fieldFilter);
+        }
+
+        /// <summary>
+        /// Adds field interception support intercepting all static fields on the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>     
+        public static void InterceptAllStaticFields(this IReflectionStructureVisitable targetType)
+        {
+            var methodFilter = GetMethodFilter();
+            Func<FieldReference, bool> fieldFilter = field =>
+            {
+                var actualField = field.Resolve();
+                return !actualField.IsStatic;
+            };
+
+            targetType.InterceptFields(methodFilter, fieldFilter);
+        }
+
+        /// <summary>
+        /// Adds field interception support to the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>        
+        public static void InterceptAllFields(this IReflectionVisitable targetType)
+        {
+            var methodFilter = GetMethodFilter();
+            targetType.InterceptFields(methodFilter, f => true);
+        }
+
+        /// <summary>
+        /// Adds field interception support intercepting all instance fields on the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>        
+        public static void InterceptAllInstanceFields(this IReflectionVisitable targetType)
+        {
+            var methodFilter = GetMethodFilter();
+            Func<FieldReference, bool> fieldFilter = field =>
+                                                         {
+                                                             var actualField = field.Resolve();
+                                                             return !actualField.IsStatic;
+                                                         };
+
+            targetType.InterceptFields(methodFilter, fieldFilter);
+        }
+
+        /// <summary>
+        /// Adds field interception support intercepting all static fields on the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>     
+        public static void InterceptAllStaticFields(this IReflectionVisitable targetType)
+        {
+            var methodFilter = GetMethodFilter();
+            Func<FieldReference, bool> fieldFilter = field =>
+            {
+                var actualField = field.Resolve();
+                return !actualField.IsStatic;
+            };
+
+            targetType.InterceptFields(methodFilter, fieldFilter);
+        }
+
+        /// <summary>
+        /// Adds field interception support to the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>
+        /// <param name="methodFilter">The filter that determines which methods on the target type will be modified to support field interception.</param>
+        /// <param name="fieldFilter">The filter that determines which fields should be intercepted.</param>
+        public static void InterceptFields(this IReflectionVisitable targetType, Func<MethodReference, bool> methodFilter, Func<FieldReference, bool> fieldFilter)
+        {
+            var typeWeaver = new ImplementFieldInterceptionHostWeaver(t => true);
+            var fieldWeaver = new InterceptFieldAccess(fieldFilter);
+
+            targetType.WeaveWith(fieldWeaver, methodFilter);
+            targetType.Accept(typeWeaver);
+        }
+
+        /// <summary>
+        /// Adds field interception support to the target type.
+        /// </summary>
+        /// <param name="targetType">The type that will be modified.</param>
+        /// <param name="methodFilter">The filter that determines which methods on the target type will be modified to support field interception.</param>
+        /// <param name="fieldFilter">The filter that determines which fields should be intercepted.</param>
+        public static void InterceptFields(this IReflectionStructureVisitable targetType, Func<MethodReference, bool> methodFilter, Func<FieldReference, bool> fieldFilter)
+        {
+            var typeWeaver = new ImplementFieldInterceptionHostWeaver(t => true);
+            var fieldWeaver = new InterceptFieldAccess(fieldFilter);
+
+            targetType.WeaveWith(fieldWeaver, methodFilter);
+            targetType.Accept(typeWeaver);
+        }
+
+        private static Func<TypeReference, bool> GetTypeFilter()
+        {
+            return type =>
+            {
+                var actualType = type.Resolve();
+                if (actualType.IsValueType || actualType.IsInterface || actualType.Name == "<Module>")
+                    return false;
+
+                return actualType.IsClass;
+            };
+        }
+
+        private static Func<MethodReference, bool> GetMethodFilter()
+        {
+            return m => m.Name != ".cctor" && m.Name != ".ctor";
+        }
+    }
+}
---
+++ src/LinFu.AOP/Interfaces/ITypeFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+namespace LinFu.AOP.Cecil
+{
+    /// <summary>
+    /// Represents a type that determines whether or not a particular type should be modified.
+    /// </summary>
+    public interface ITypeFilter
+    {
+        /// <summary>
+        /// Determines whether or not a type should be modified.
+        /// </summary>
+        /// <param name="type">The target type.</param>
+        /// <returns>Returns <c>true</c> if the type should be modified.</returns>
+        bool ShouldWeave(TypeDefinition type);
+    }
+}
