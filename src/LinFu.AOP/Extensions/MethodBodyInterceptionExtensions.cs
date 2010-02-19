e836facf351e61f998d9e1cec5561ce76978a7af
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2010-02-19 04:20:31 +0000

    Added missing files
    git-svn-id: http://linfu.googlecode.com/svn/trunk@488 228fa388-fe3e-0410-9462-c1833096a2b3

---
+++ src/LinFu.AOP/Extensions/MethodBodyInterceptionExtensions.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+namespace LinFu.AOP.Cecil
+{
+    /// <summary>
+    /// Represents an extension class that adds method body interception support to the Mono.Cecil object model.
+    /// </summary>
+    public static class MethodBodyInterceptionExtensions
+    {
+        public static void InterceptAllMethodBodies(this IReflectionStructureVisitable target)
+        {
+            target.InterceptMethodBody(m => true);
+        }
+
+        public static void InterceptAllMethodBodies(this IReflectionVisitable target)
+        {
+            target.InterceptMethodBody(m => true);
+        }
+
+        public static void InterceptMethodBody(this IReflectionStructureVisitable target, Func<MethodReference, bool> methodFilter)
+        {
+            var typeFilter = GetTypeFilter();
+            target.Accept(new ImplementModifiableType(typeFilter));
+
+            var interceptMethodBody = new InterceptMethodBody(methodFilter);
+            target.WeaveWith(interceptMethodBody, methodFilter);
+        }        
+
+        public static void InterceptMethodBody(this IReflectionVisitable target, Func<MethodReference, bool> methodFilter)
+        {
+            var typeFilter = GetTypeFilter();
+            target.Accept(new ImplementModifiableType(typeFilter));
+            
+            var interceptMethodBody = new InterceptMethodBody(methodFilter);
+            target.WeaveWith(interceptMethodBody, methodFilter);
+        }
+
+        private static Func<TypeReference, bool> GetTypeFilter()
+        {
+            return type =>
+            {
+                var actualType = type.Resolve();
+                if (actualType.IsValueType || actualType.IsInterface)
+                    return false;
+
+                return actualType.IsClass;
+            };
+        }
+    }
+}
