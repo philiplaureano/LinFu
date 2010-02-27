99f960abdcaff1501d1be3e3704a3ae68d36eff8
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2010-02-27 22:01:50 +0000

    Added missing files
    git-svn-id: http://linfu.googlecode.com/svn/trunk@497 228fa388-fe3e-0410-9462-c1833096a2b3

---
+++ src/LinFu.AOP.Interfaces/AroundInvokeMethodCallRegistry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.AOP.Interfaces
+{
+    public static class AroundInvokeMethodCallRegistry
+    {
+        private static readonly List<IAroundInvokeProvider> _providers = new List<IAroundInvokeProvider>();
+        private static readonly object _lock = new object();
+
+        public static IAroundInvoke GetSurroundingImplementation(IInvocationInfo context)
+        {
+            var resultList = (from p in _providers
+                              where p != null
+                              let aroundInvoke = p.GetSurroundingImplementation(context)
+                              where aroundInvoke != null
+                              select aroundInvoke).ToList();
+
+            if (resultList.Count == 0)
+                return null;
+
+            return new CompositeAroundInvoke(resultList);
+        }
+
+        public static void AddProvider(IAroundInvokeProvider provider)
+        {
+            lock (_lock)
+            {
+                _providers.Add(provider);
+            }
+        }
+
+        public static void Clear()
+        {
+            lock (_lock)
+            {
+                _providers.Clear();
+            }
+        }
+    }
+}
---
+++ src/LinFu.AOP.Interfaces/BootstrapException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.AOP.Interfaces
+{
+    [Serializable]
+    public class BootstrapException : Exception
+    {
+        public BootstrapException(string message, Exception ex) : base(message, ex)
+        {                        
+        }
+    }
+}
