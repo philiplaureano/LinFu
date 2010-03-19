decd9dd782e20eaf17f32934b9810e59f9b47737
Philip Laureano <philip.laureano@gmail.com>
2010-03-19 09:26:26 +0800

    Updated the postweaver console project to use the consolidated LinFu.Core assembly

---
+++ src/UnitTests/EnumerableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.UnitTests
+{
+    public static class EnumerableExtensions
+    {
+        public static bool AreEqualTo<T>(this IEnumerable<T> expectedArray, IEnumerable<T> actualArray)
+        {
+            var areEqual = true;
+
+            var expectedItems = new Queue<T>(expectedArray);
+            var actualItems = new Queue<T>(actualArray);
+
+            if (expectedItems.Count != actualItems.Count)
+                return false;
+
+            while (expectedItems.Count > 0)
+            {
+                var expectedItem = expectedItems.Dequeue();
+                var actualItem = actualItems.Dequeue();
+
+                if (Equals(expectedItem, actualItem))
+                    continue;
+
+                areEqual = false;
+                break;
+            }
+
+            return areEqual;
+        }
+    }
+}
