1c412207569977c5e95766f3d2f51ff31a708702
Philip Laureano <philip.laureano@gmail.com>
2010-11-16 08:08:32 +0800

    Fixed a threading bug in the Initializer<T> class that was caused by not wrapping the _instances field with a lock statement

--- src/LinFu.IoC/Configuration/InitializerOfT.cs
+++ src/LinFu.IoC/Configuration/InitializerOfT.cs
@@ -14,6 +14,8 @@ namespace LinFu.IoC.Configuration
         private static readonly HashSet<HashableWeakReference> _instances =
             new HashSet<HashableWeakReference>(new HashableWeakReferenceComparer());
 
+        private static readonly object _lock = new object();
+
         private static int _initializeCallCount;
         private readonly Func<IServiceRequestResult, T> _getSource;
 
@@ -59,17 +61,22 @@ namespace LinFu.IoC.Configuration
             if (target == null)
                 return;
 
-            if ((_initializeCallCount = ++_initializeCallCount%100) == 0)
-                _instances.RemoveWhere(w => !w.IsAlive);
+            lock (_lock)
+            {
 
-            // Make sure that the target is initialized only once
-            var weakReference = new HashableWeakReference(target);
-            if (_instances.Contains(weakReference))
-                return;
 
-            // Initialize the target
-            target.Initialize(source);
-            _instances.Add(weakReference);
+                if ((_initializeCallCount = ++_initializeCallCount % 100) == 0)
+                    _instances.RemoveWhere(w => !w.IsAlive);
+
+                // Make sure that the target is initialized only once
+                var weakReference = new HashableWeakReference(target);
+                if (_instances.Contains(weakReference))
+                    return;
+
+                // Initialize the target
+                target.Initialize(source);
+                _instances.Add(weakReference);
+            }
         }
 
         #region Nested type: HashableWeakReference
@@ -78,7 +85,8 @@ namespace LinFu.IoC.Configuration
         {
             private readonly int _hashCode;
 
-            public HashableWeakReference(object target) : base(target, false)
+            public HashableWeakReference(object target)
+                : base(target, false)
             {
                 _hashCode = target.GetHashCode();
             }
